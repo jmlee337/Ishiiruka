fb8d864bc53e1dfed4c772bd07b32c65859d12df
Nicolet <jmlee337@gmail.com>
2025-03-02 01:19:06 -0600

    add logging to avidump

--- Source/Core/VideoCommon/AVIDump.cpp
+++ Source/Core/VideoCommon/AVIDump.cpp
@@ -160,6 +160,28 @@ bool AVIDump::CreateVideoFile()
 
 	File::CreateFullPath(s_dump_path);
 
+	AVOutputFormat *nextFormat = av_oformat_next(NULL);
+	while (nextFormat != NULL) {
+		ERROR_LOG(VIDEO, "Format: %s, %s", nextFormat->name, nextFormat->mime_type);
+		nextFormat = av_oformat_next(nextFormat);
+	}
+
+	const AVCodecDescriptor *nextCodec = avcodec_descriptor_next(NULL);
+	while (nextCodec != NULL) {
+		if (nextCodec->type == AVMEDIA_TYPE_VIDEO) {
+			ERROR_LOG(VIDEO, "Codec: %s", nextCodec->name);
+		}
+		nextCodec = avcodec_descriptor_next(nextCodec);
+	}
+
+	const AVCodec *nextEncoder = av_codec_next(NULL);
+	while (nextEncoder != NULL) {
+		if (nextEncoder->type == AVMEDIA_TYPE_VIDEO && av_codec_is_encoder(nextEncoder)) {
+			ERROR_LOG(VIDEO, "Encoder: %s", nextEncoder->name);
+		}
+		nextEncoder = av_codec_next(NULL);
+	}
+
 	auto* output_format = av_guess_format(s_format.c_str(), s_dump_path.c_str(), nullptr);
 	if (!output_format)
 	{
